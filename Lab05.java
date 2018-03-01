596e33ae926ad8dceb276563b34b7adec7939e43
pernudg8329 <pernudg8329@student.laccd.edu>
2018-03-01 15:57:36 -0500

    Lab05 - Correcting Logic Mistakes

--- Lab05.java
+++ Lab05.java
@@ -22,13 +22,13 @@ public class Lab05 {
     
     //ask the user for their name.
     System.out.print("Enter in your name. Hit enter when you're done typing in your name.\n");
-    //Scanner object to get input from user.
-    name = input.next();
-      
-    //get the amount of characters in the name and store them into nameLength to test condition again for the while loop.
-    nameLength = name.length();  
     
-    do { //runs the program at least once 
+    do {
+      //runs the program at least once 
+      //prompt the user to enter in their names again.
+      name = input.next(); //user will enter in their names again
+      nameLength = name.length(); //amount of letters will be stored in Length variable 
+      
       while(nameLength < 2) {
         
         System.out.print("The name you've entered is less than 2 characters.\n"
@@ -37,23 +37,23 @@ public class Lab05 {
         //if name is less than 2, the user will enter in a new name.
         name = input.next();
         //new name will have its length stored in sme variable. 
-        nameLength = name.length();  
+        nameLength = name.length(); 
       }
       
       for(int index = 0; name.length() > index; index++) { //initalized for loop to check every single character.       
         if(!Character.isLetter(name.charAt(index))) { //if current index is not a character, prompt the user to enter their names again, 
           //show errors and repeat the loop
-          flag = true; //set the condition to true in order to run the loop again. 
+          flag = false; //set the condition to true in order to run the loop again. 
           System.out.print(name.charAt(index) + " is not a letter.\n" //displays to the user, the current index that is not a character.
                             + "Please enter in a name without numbers or illegal characters.\n");
-          //prompt the user to enter in their names again.
-          name = input.next(); //user will enter in their names again
-          nameLength = name.length(); //amount of letters will be stored in Length variable 
-          break; //break out of loop to prevent infinite-loops
+          nameLength = 0;
+          break;
         }
-        flag = false; //Set flag to false to exit out of do-while loop if user meets the two criterias.
+        else {
+        flag = true; //Set flag to false to exit out of do-while loop if user meets the two criterias.
+        }
       }
-    } while(flag == true && nameLength < 2); //check flag condition to see if user meets all of the criterias. 
+    } while(flag == false && nameLength < 2); //check flag condition to see if user meets all of the criterias. 
     System.out.println("That's a nice name.");
   } 
 }
