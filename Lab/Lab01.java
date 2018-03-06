c46b23995b29b1e99dc568696cfb73fa5f4aace5
pernudg8329 <pernudg8329@student.laccd.edu>
2018-03-06 16:26:47 -0500

    Lab01 - Test Print Splash Screen

---
+++ Lab/Lab01.java
@@ -0,0 +1,22 @@
+//Giovanni Pernudi
+//CO Sci 290
+//Testing different print examples
+
+ /* 
+  This application is a demo to test different
+  printout statements.
+  */
+ 
+public class TestPrint{
+  
+  //main method, where the application starts.
+  public static void main(String[]args){
+    System.out.println(" __________            ___.    ._              ._.   \n"
+                     + " \\____   /____   _____ \\_ |__ |__| ____   _____| |    \n"
+                     + "  /     //  _ \\ /     \\| __ \\|  |/ __\\ /  ___/ |    \n"
+                     + " /     /(  <_> )  Y Y  \\ \\_\\ \\  \\  ___/ \\___ \\ \\|  \n"
+                     + "/_______ \\____/|__|_|  /___  /__|\\___  >____  >__  \n"
+                     + "        \\/           \\/    \\/        \\/     \\/ \\/ ");
+  }
+  
+}
